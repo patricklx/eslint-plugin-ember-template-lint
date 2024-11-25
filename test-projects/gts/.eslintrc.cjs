fbf6609db8137c4c350df94a6e09e2c4a45d48e8
patrickpircher <pa.trickjlp@gmail.com>
2024-11-25 23:15:14 +0100

    copy test dirs

---
+++ test-projects/gts/.eslintrc.cjs
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname
+  },
+  overrides: [
+    {
+      files: ['**/*.{js,ts}'],
+      plugins: ['ember'],
+      parser: 'ember-eslint-parser',
+      extends: ['eslint:recommended', 'plugin:ember/recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
+    },
+    {
+      files: ['**/*.gts'],
+      parser: 'ember-eslint-parser',
+      plugins: ['ember'],
+      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'plugin:ember/recommended', 'plugin:ember/recommended-gts'],
+    },
+    {
+      files: ['**/*.gjs'],
+      parser: 'ember-eslint-parser',
+      plugins: ['ember'],
+      extends: [
+        'eslint:recommended',
+        'plugin:ember/recommended',
+        'plugin:ember/recommended-gjs',
+        'plugin:ember-template-lint/config',
+        'plugin:ember-template-lint/recommended',
+      ],
+    },
+  ],
+};
